f60f1994dc3eb7f28852afe94697b151e84af0ed
李木 <limuxljy@dingding.com>
2018-01-03 17:43:17 +0800

    增加升级到router4

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import { Router, Route, IndexRoute, hashHistory } from "react-router";
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+  } from 'react-router-dom'
 import { useStrict } from "mobx";
 import { Provider } from "mobx-react";
 
 // components
-import App from './components/App'
+import App from './pages/App'
+import Login from './pages/Login'
 
 
 //stores
@@ -20,8 +25,19 @@ useStrict(true);
 
 const Root = () =>
 <Provider {...stores}>
-    <Router history={hashHistory}>
-        <Route path="/" component={App}>
+    <Router>
+            {/* <Route path="/" exact component={App}></Route> */}
+            <div>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+                <div>
+                    <Route path="/" exact component={App}></Route>
+                    <Route path="/login" exact component={Login}></Route>
+                </div>
+            </div>
+            {/* <Route path="/" component={Login}></Route> */}
             {/*<IndexRoute component={ListBooks} />
              <Route path="login" component={Login} />
             <Route path="register" component={Register} />
@@ -31,7 +47,6 @@ const Root = () =>
             <Route path="settings" component={Settings} />
             <Route path="@:username" component={Profile} />
             <Route path="@:username/favorites" component={Profile} /> */}
-        </Route>
     </Router>
 </Provider>;
 
---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Login extends Component {
+  constructor () {
+    super()
+  }
+
+  render () {
+    return (<h1>Login</h1>)
+  }
+}
+
+export default Login
